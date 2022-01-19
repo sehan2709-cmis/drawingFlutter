e2aef715fc86d5c2959111ef5ec0b347ba97467c
Sehan Kwon <sehankwon@Sehanui-MacBookPro.local>
2022-01-19 16:47:58 +0900

    I added sliding thing/bar

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Color selectedColor = Colors.black;
   double strokeWidth = 2.0;
 
+
+  Widget close = Icon(Icons.close, color: Colors.white);
+
   @override
   void initState() {
     super.initState();
@@ -53,14 +56,41 @@ class _MyHomePageState extends State<MyHomePage> {
           return AlertDialog(
             title: const Text('Color Chooser'),
             content: SingleChildScrollView(
-              child: BlockPicker(
+              // child: BlockPicker(
+              //   pickerColor: selectedColor,
+              //   onColorChanged: (color) {
+              //     this.setState(() {
+              //       selectedColor = color;
+              //     });
+              //   },
+              // ),
+
+
+              child: ColorPicker(
                 pickerColor: selectedColor,
                 onColorChanged: (color) {
-                  this.setState(() {
-                    selectedColor = color;
-                  });
+                  this.setState(
+                    () {
+                      selectedColor = color;
+                    },
+                  );
                 },
+                showLabel: true,
+                pickerAreaHeightPercent: 0.8,
               ),
+
+            // child: MaterialPicker(
+            //    pickerColor: selectedColor,
+            //    onColorChanged: (color) {
+            //           this.setState(
+            //             () {
+            //               selectedColor = color;
+            //             },
+            //           );
+            //         },
+            //    showLabel: true, // only on portrait mode
+             //),
+
             ),
             actions: <Widget>[
               FlatButton(
@@ -77,6 +107,32 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
     final double height = MediaQuery.of(context).size.height;
+    List<Widget> menus = [
+      IconButton(
+          onPressed: () {
+            selectColor();
+          },
+          icon: const Icon(
+            Icons.color_lens,
+          )),
+      Slider(
+          min: 1.0,
+          max: 7.0,
+          value: strokeWidth,
+          onChanged: (value) {
+            setState(() {
+              strokeWidth = value;
+            });
+          }
+      ),
+      IconButton(
+          onPressed: () {
+            this.setState(() {
+              points.clear();
+            });
+          },
+          icon: const Icon(Icons.layers_clear)),
+    ];
 
     return Scaffold(
       body: Stack(
@@ -156,49 +212,22 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: height * 0.01,
+                Positioned(
+                  right: 0,
+                  left: 0,
+                  bottom: 20.0,
+                  child: JamesBar(
+                      color: Colors.orange,
+                      closeIcon: this.close,
+                      items: menus,
+                      cb: (int i) => print(i)),
                 ),
-                Container(
-                  width: width * 0.80,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  ),
-                  child: Row(
-                    children: [
-                      IconButton(
-                          onPressed: () {
-                            selectColor();
-                          },
-                          icon: Icon(
-                            Icons.color_lens,
-                          )),
-                      Expanded(
-                          child: Slider(
-                              min: 1.0,
-                              max: 7.0,
-                              value: strokeWidth,
-                              onChanged: (value) {
-                                this.setState(() {
-                                  strokeWidth = value;
-                                });
-                              })),
-                      IconButton(
-                          onPressed: () {
-                            this.setState(() {
-                              points.clear();
-                            });
-                          },
-                          icon: const Icon(Icons.layers_clear)),
-                    ],
-                  ),
-                )
               ],
             ),
           )
         ],
       ),
+
     );
   }
 }
@@ -233,4 +262,107 @@ class MyCustomPainter extends CustomPainter {
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
   }
+}
+
+
+class JamesBar extends StatefulWidget {
+  final Axis axis;
+  final Widget closeIcon;
+  final List<Widget> items;
+  final void Function(int) cb;
+  final Color color;
+
+  const JamesBar({
+    Key? key,
+    this.axis = Axis.horizontal,
+    required this.closeIcon,
+    required this.items,
+    required this.cb,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  State<JamesBar> createState() => _JamesBarState();
+}
+
+class _JamesBarState extends State<JamesBar>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _act;
+  Animation<double>? _anim;
+
+  bool check = false;
+
+  @override
+  void initState() {
+    this._act =
+    AnimationController(vsync: this, duration: Duration(milliseconds: 300))
+      ..addListener(() {
+        if (!this.mounted) return;
+        setState(() {});
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    this._act?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => this._horizontal(context);
+
+  Widget _trigger() => IconButton(
+    icon: this.widget.closeIcon as Icon,
+    onPressed: () {
+      if (!check) {
+        this._act?.forward();
+      } else {
+        this._act?.reverse();
+      }
+      this.check = !this.check;
+    },
+  );
+
+  Widget _horizontal(BuildContext context) {
+    this._anim =
+        Tween<double>(begin: MediaQuery.of(context).size.width * 0.87, end: 0)
+            .animate(_act!);
+    return Transform.translate(
+      offset: Offset(this._anim?.value ?? 0, 0),
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 60.0),
+        shape:
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
+        elevation: 4.0,
+        color: this.widget.color,
+        child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            height: 80.0,
+            child: Container(
+              child: Row(
+                children: [
+                  Container(child: this._trigger()),
+                  Expanded(
+                    child: Row(
+                      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [...this._bar()],
+                    ),
+                  ),
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+
+  List<Widget> _bar() => this
+      .widget
+      .items
+      .map<Widget>((Widget item) => InkWell(
+    onTap: () => this.widget.cb(widget.items.indexOf(item)),
+    child: item,
+  ))
+      .toList();
 }
